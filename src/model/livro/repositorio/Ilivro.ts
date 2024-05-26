9a2550b974f36f67fae4be9e9c21a2c885732a30
matutaj <matutajorge2002@gmail.com>
2024-05-26 20:03:29 +0100

    add interface de livro

---
+++ src/model/livro/repositorio/Ilivro.ts
@@ -0,0 +1,22 @@
+import { Categoria, Livro } from "@prisma/client";
+
+export interface ILivroData{
+    id?:string,
+    nome:string,
+    autor:string,
+    dataLancamento:Date,
+    preco:number,
+    quantidade:number,
+    descricao?:string,
+    categoriaId:string
+
+}
+export interface Ilivro{
+    criar({}:ILivroData):Promise<Livro>
+    listarTodoLivro():Promise<Livro []>
+    listarLivroPeloNome(nome:string):Promise<Livro |undefined>
+    listarLivroPeloId(id:string):Promise< Livro | undefined>
+    listarPorCategoria(categoriaId:string):Promise<Categoria | undefined>
+    eliminarLivro(id:string):Promise<void>
+    atualizar({}:ILivroData):Promise<Livro>
+}
