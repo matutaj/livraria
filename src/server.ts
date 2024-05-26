0c2857b96d7af9064a5c48abedcb467c0efe23fe
matutaj <matutajorge2002@gmail.com>
2024-05-26 19:20:16 +0100

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express"
+import { routes } from "./routes";
+import AppErrorHandler from "./errors/errorhandler";
 
 const app = express();
 
 const port = 2222;
 
 app.use(express.json());
+
+app.use(routes);
+app.use(AppErrorHandler)
 
 app.listen(port, ()=>{
     console.log("Livraria do Itel")
